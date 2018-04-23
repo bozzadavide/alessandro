54de3aca2e6b1af1e93ed286857c4d35732d354f
Davide Bozza <davide.bozza@infocert.it>
2018-04-23 18:54:06 +0200

    First Commit

---
+++ mensaserver/src/main/java/it/alessandrosenesi/mensaserver/App.java
@@ -0,0 +1,13 @@
+package it.alessandrosenesi.mensaserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
